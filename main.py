91f96106244fd59ccfb62153ef186cb19aeb3760
Gabriel Sakali <gabedevs@gmail.com>
2025-04-01 11:07:12 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+score = 0
+questions = [
+    {"1": "What is the capital of Italy?", "answer": "rome"},
+    {"2": "From what language are the words 'a la carte' being referenced from?", "answer": "french"},
+    {"3": "From which continent is Bosnia from?", "answer": "europe"},
+    {"4": "Name the country north of England", "answer": "scotland"},
+    {"5": "Which prominent sea is south of Spain? (one word)", "answer": "mediterranean"},
+    {"6": "What was the first name of France's first emperor?", "answer": "napoleon"},
+    {"7": "What is the country between the border of France and Spain?", "answer": "andorra"},
+    {"8": "Is germany landlocked? (one word)", "answer": "no"},
+    {"9": "How many countries does the Rhine flow through? (one word, no digits)", "answer": "six"},
+    {"10": "Which year did the Berlin wall get demolished? (digits only)", "answer": "1989"},
+]
